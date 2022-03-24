455254fdfaad64de9d9f7f13b2b7d712872e7008
MrBallonvas <vasilisrusgame@gmail.com>
2022-03-24 22:38:56 +0500

    1 comm

---
+++ main.py
@@ -0,0 +1,48 @@
+import telebot
+from telebot import types
+import requests
+
+urlYoutube = 'https://www.youtube.com/'
+urlFacebok = 'https://ru-ru.facebook.com/'
+urlInstagram = 'https://www.instagram.com/'
+urlTwitter = 'https://twitter.com/?lang=ru'
+
+def checkSocNetworks(url):
+	r = requests.get(url)
+
+	print(r.status_code)
+
+	
+
+checkSocNetworks(urlTwitter)
+
+# bot = telebot.TeleBot('5238271299:AAFK97xyBijCBDxOvX-J_q5J9TnoIa2RgvM')
+
+# @bot.message_handler(commands=['start'])
+# def start_message(message):
+# 	markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
+# 	item1=types.KeyboardButton("youtube")
+# 	item2=types.KeyboardButton("facebook")
+# 	item3=types.KeyboardButton("instagram")
+# 	item4=types.KeyboardButton("twitter")
+# 	markup.add(item1)
+# 	markup.add(item2)
+# 	markup.add(item3)
+# 	markup.add(item4)
+# 	bot.send_message(message.chat.id,"Привет, выберите сервис который вы хотите проверить.", reply_markup=markup)
+
+# @bot.message_handler(content_types='text')
+# def message_reply(message):
+# 	if message.text=="youtube":
+# 		bot.send_message(message.chat.id,"youtube")
+
+# 	if message.text=="facebook":
+# 		bot.send_message(message.chat.id,"facebook")
+
+# 	if message.text=="instagram":
+# 		bot.send_message(message.chat.id,"instagram")
+
+# 	if message.text=="twitter":
+# 		bot.send_message(message.chat.id,"twitter")
+
+# bot.polling(none_stop=True)
